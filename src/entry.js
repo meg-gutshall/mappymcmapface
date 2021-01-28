b3c73e856f65b925adf4f5028d8f3bf7a7269ef9
Meg Gutshall <meghan.gutshall@gmail.com>
2021-01-28 16:56:47 -0500

    Adjust code comment formatting

--- src/entry.js
+++ src/entry.js
@@ -1,19 +1,22 @@
 // This function runs after a successful login to render the Code Talks Topic Request Platform's main content.
 function renderTopicRequests(REQS_URL) {
-  // During the JWT authorization process, I saved the user's ID to the browser's localStorage. Line 4 accesses that ID now and assigns it to the variable `current_user`.
+  // During the JWT authorization process, I saved the user's ID to the browser's localStorage. Line 4 accesses that ID now and assigns it to
+  // the variable `current_user`.
   let current_user = localStorage.getItem('current_user');
 
-  // This function creates two Bootstrap row elements in the body of the page: one specfically meant for the current user, the other for all other users. Optional: Refer to the `domElements.js` file for further details on this.
+  // This function creates two Bootstrap row elements in the body of the page: one specifically meant for the current user, the other for all
+  // other users. Optional: Refer to the `domElements.js` file for further details on this.
   async function createRowElements() {
     DOMElements.userRow;
     DOMElements.allOtherRow;
   }
 
-  // This function fetches all topic requests from the backend database. In my case, I was using Rails with PostgreSQL.
+  // This function fetches all topic requests from the backend database. In my case, I used Rails with PostgreSQL for the backend.
   async function fetchTopicRequests(url) {
-    // Await the creation of the row elements to render. This must complete before moving forward or only two empty row containers will render.
+    // Await the creation of the row elements. This must complete before moving forward or else only two empty row containers will render on the page.
     await createRowElements();
-    // Assign options to a variable to pass in as an argument to the fetch function, including the current user's JWT token from local storage and the HTTP method.
+    // Assign options to a variable to pass in as an argument to the fetch function, including the current user's JWT token from local storage
+    // and the HTTP method.
     const jwtGetFetchOptions = {
       method: 'GET',
       headers: {Authorization: `Bearer ${localStorage.getItem('jwt_token')}`}
@@ -22,12 +25,13 @@ function renderTopicRequests(REQS_URL) {
       .then(resp => resp.json())
       .then(topicRequests => topicRequests.data.forEach(attrs => {
         let newTopicRequest = new TopicRequest(attrs);
-        // Check the `newTopicRequest` variable's `codepanionId` property to see if it matches the current user's ID which was stored in the `current_user` variable above.
+        // Check the `newTopicRequest` variable's `codepanionId` property to see if it matches the current user's ID which was stored in the
+        // `current_user` variable above.
         if (newTopicRequest.codepanionId == parseInt(current_user)) {
-          // If truthy, call the class method `renderTopicRequests` passing in the `'user-row'` argument.
+          // If truthy, call the class method `renderTopicRequests` on the newly created TopicRequest object, passing in the `'user-row'` argument.
           newTopicRequest.renderTopicRequests('user-row');
         } else {
-          // If falsy, call the class method `renderTopicRequests` passing in the `'all-other-row'` argument.
+          // If falsy, call the class method `renderTopicRequests` on the newly created TopicRequest object, passing in the `'all-other-row'` argument.
           newTopicRequest.renderTopicRequests('all-other-row');
         }
       }));
--- src/models/topicRequest.js
+++ src/models/topicRequest.js
@@ -1,8 +1,13 @@
-// This class also contains a constructor method and a method called `createTopicRequestCard` which returns an HTML element formatted as a Bootstrap card that contains details about the TopicRequest object.
+// This class also contains a constructor method and a method called `createTopicRequestCard` which returns an HTML element formatted as a
+// Bootstrap card that contains details about the TopicRequest object.
 
 class TopicRequest {
-  // The `renderTopicRequests` method acts as a mini factory to insert new topic request cards into their proper rows. As you can see on line 26 in `src/entry.js`, after a new TopicRequest object is created, it's fed through a conditional statement to see whether or not the request belongs to the current user. The result determines which elementId will be passed as an argument to `renderTopicRequests`.
-  // Previously this function was duplicated (one for current user and one for all other users) and lived in `src/entry.js`. Now as a class function, it dynamically retrieves the correct row from the DOM, creates a Bootstrap card to represent the new TopicRequest object, and inserts that card as a column in the selected row. This refactored method is performant and has more than halved the code for this single action.
+  // The `renderTopicRequests` method acts as a mini factory to insert new topic request cards into their proper rows. As you can see on
+  // line 26 in `src/entry.js`, after a new TopicRequest object is created, it's fed through a conditional statement to see whether or not the request belongs
+  // to the current user. The result determines which elementId will be passed as an argument to `renderTopicRequests`.
+  // Previously this function was duplicated (one for current user and one for all other users) and lived in `src/entry.js`. Now as a class function,
+  // it dynamically retrieves the correct row from the DOM, creates a Bootstrap card to represent the new TopicRequest object, and inserts that card as a
+  // column in the selected row. This refactored method is performant and has more than halved the code for this single action.
   renderTopicRequests(elementId) {
     let row = document.getElementById(elementId);
     let col = this.createTopicRequestCard();
